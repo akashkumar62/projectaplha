5579c0157363c79ea1d3a1999be4c35a5013f20e
akashkumar62 <xiome555@gmail.com>
2026-01-31 12:15:40 +0530

    elastic-service added

---
+++ elastic-search-service/src/main/java/com/elasticsearch/elastic_search_service/ElasticSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.elastic_search_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchServiceApplication.class, args);
+	}
+
+}
