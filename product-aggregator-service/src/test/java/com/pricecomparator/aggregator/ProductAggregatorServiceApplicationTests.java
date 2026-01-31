28338b980ac6a1f4c9381064e0314db7a47fd4d3
akashkumar62 <xiome555@gmail.com>
2026-01-31 11:50:21 +0530

    initial commit

---
+++ product-aggregator-service/src/test/java/com/pricecomparator/aggregator/ProductAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricecomparator.aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
